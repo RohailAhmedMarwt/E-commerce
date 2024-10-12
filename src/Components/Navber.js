708ccc45262c8d46b74d1593bf0468d5ca6e031a
RohailMarwat434 <sohailahmad348458@gmail.com>
2024-10-12 16:29:38 -0700

    vacvakcabv xhja

--- src/Components/Navber.js
+++ src/Components/Navber.js
@@ -24,7 +24,7 @@ const Navber = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-body shadow p-2  bg-white rounded">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="#"><img src="/logo4.png" alt="" style={{ width: "90px" }} /></Link>
+          <Link className="navbar-brand" to="#"><img src="/logo6.jpeg" alt="" style={{ width: "90px" }} /></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
